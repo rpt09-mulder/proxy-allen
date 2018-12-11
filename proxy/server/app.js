da0f0fc36d1dda563c17c8dbeee519886e1d9733
Allen Hsieh <allenhsiehhr@gmail.com>
2018-12-10 17:55:42 -0800

    Added morgan for logging

--- proxy/server/app.js
+++ proxy/server/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const morgan = require('morgan');
 
 const app = express();
+
+app.use(morgan('dev'));
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
@@ -10,7 +13,7 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/../public')));
 
 app.get('/rooms/:id', (req, res) => {
-  res.send('you hit roomID', req.params.id)
+  res.send(`you hit roomID ${req.params.id}`);
 });
 
 module.exports = app;
