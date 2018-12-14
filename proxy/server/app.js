1dec1332cb5023ea06618e90bf3a2e48427dc5d6
Allen Hsieh <allenhsiehhr@gmail.com>
2018-12-13 20:08:56 -0800

    Deleted react/reactdom and render scripts

--- proxy/server/app.js
+++ proxy/server/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const morgan = require('morgan');
+const axios = require('axios');
 
 const app = express();
 
@@ -11,9 +12,16 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, '/../public')));
+app.use('/:id', express.static(path.join(__dirname, '/../public')));
 
-app.get('/rooms/:id', (req, res) => {
-  res.send(`you hit roomID ${req.params.id}`);
-});
+// app.get('/rooms/:id', (req, res) => {
+  // res.send(`you hit roomID ${req.params.id}`);
+  // let {id} = req.params;
+  // axios.get(`http://localhost:3001/${req.url}`)
+  //   .then((axiosresponse) => {
+  //     console.log('axios res', axiosresponse.data);
+  //     res.send(axiosresponse.data);
+  //   });
+// });
 
 module.exports = app;
