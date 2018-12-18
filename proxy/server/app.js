2122942882ca43b62af4a38325d9c66b27c03708
Allen Hsieh <10136383+allenhsieh@users.noreply.github.com>
2018-12-17 21:13:14 -0800

    Deployed to AWS via Elastic Beanstalk (#8)
    * Using Philipps scaffolding
    
    * Added philipps stylesheet
    
    * added server start script
    
    * Added EB init file
    
    * Removed a bunch of dependencies i don't need
    Took them from component before realizing what proxy is doing
    
    * Added config files for EB

--- proxy/server/app.js
+++ proxy/server/app.js
@@ -1,27 +1,12 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const path = require('path');
 const morgan = require('morgan');
-const axios = require('axios');
 
 const app = express();
 
 app.use(morgan('dev'));
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
-
 app.use(express.static(path.join(__dirname, '/../public')));
 app.use('/:id', express.static(path.join(__dirname, '/../public')));
-
-// app.get('/rooms/:id', (req, res) => {
-  // res.send(`you hit roomID ${req.params.id}`);
-  // let {id} = req.params;
-  // axios.get(`http://localhost:3001/${req.url}`)
-  //   .then((axiosresponse) => {
-  //     console.log('axios res', axiosresponse.data);
-  //     res.send(axiosresponse.data);
-  //   });
-// });
 
 module.exports = app;
