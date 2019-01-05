3ed76e858434de492da87ae919577127c7f302fa
Allen Hsieh <allenhsiehhr@gmail.com>
2019-01-05 12:10:58 -0800

    changed maxAge to string for posterity

--- proxy/server/app.js
+++ proxy/server/app.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(morgan('dev'));
 
-app.use(express.static(path.join(__dirname, '/../public'), {maxAge: 31536000}));
-app.use('/:id', express.static(path.join(__dirname, '/../public'), {maxAge: 31536000}));
+app.use(express.static(path.join(__dirname, '/../public'), {maxAge: '1y'}));
+app.use('/:id', express.static(path.join(__dirname, '/../public'), {maxAge: '1y'}));
 
 module.exports = app;
