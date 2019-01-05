f49695a23e1fdf52423eddeedbddc0b58dad2d6d
Allen Hsieh <allenhsiehhr@gmail.com>
2019-01-05 12:07:01 -0800

    implemented cache per google insights

--- proxy/server/app.js
+++ proxy/server/app.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(morgan('dev'));
 
-app.use(express.static(path.join(__dirname, '/../public')));
-app.use('/:id', express.static(path.join(__dirname, '/../public')));
+app.use(express.static(path.join(__dirname, '/../public'), {maxAge: 31536000}));
+app.use('/:id', express.static(path.join(__dirname, '/../public'), {maxAge: 31536000}));
 
 module.exports = app;
